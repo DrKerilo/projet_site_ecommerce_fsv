710452b9d04f57779ec8c084f6d2fec7183bec6f
inti0331 <inti0331@DESKTOP-HLSFT8K>
2018-04-09 14:33:01 +0200

    Val: commit Produit

--- Projet_Site_Ecommerce/src/main/java/fr/adaming/model/Produit.java
+++ Projet_Site_Ecommerce/src/main/java/fr/adaming/model/Produit.java
@@ -3,6 +3,7 @@ package fr.adaming.model;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -41,8 +42,95 @@ public class Produit implements Serializable {
 	@JoinColumn(name="cat_id", referencedColumnName="id_cat")
 	private Categorie categorie;
 	// OneToMany avec LigneCommande (+ si on supprimer le produit, on supprime les LigneCommande
-	@OneToMany(mappedBy="produit")
+	@OneToMany(mappedBy="produit", cascade=CascadeType.REMOVE)
 	private List<LigneCommande> listeLignesCommandes;
 	
-
+	// Constructeurs
+	public Produit() {
+		super();
+	}
+	public Produit(String nomProduit, String description, double prix, int quantite, boolean selectionne,
+			byte[] photo) {
+		super();
+		this.nomProduit = nomProduit;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	public Produit(Long id, String nomProduit, String description, double prix, int quantite, boolean selectionne,
+			byte[] photo) {
+		super();
+		this.id = id;
+		this.nomProduit = nomProduit;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	
+	// Getters et setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomProduit() {
+		return nomProduit;
+	}
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+	public byte[] getPhoto() {
+		return photo;
+	}
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public Categorie getCategorie() {
+		return categorie;
+	}
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+	public List<LigneCommande> getListeLignesCommandes() {
+		return listeLignesCommandes;
+	}
+	public void setListeLignesCommandes(List<LigneCommande> listeLignesCommandes) {
+		this.listeLignesCommandes = listeLignesCommandes;
+	}
+	
 }
