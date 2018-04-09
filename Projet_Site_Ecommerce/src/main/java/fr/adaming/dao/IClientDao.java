f358056d08dcbda4a45ea1991c8512b0535e78b8
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-04-09 14:14:31 +0200

    Fab : Client modele

---
+++ Projet_Site_Ecommerce/src/main/java/fr/adaming/dao/IClientDao.java
@@ -0,0 +1,8 @@
+package fr.adaming.dao;
+
+import fr.adaming.model.Client;
+
+public interface IClientDao {
+	public Client addClient(Client cl);
+
+}
--- Projet_Site_Ecommerce/src/main/java/fr/adaming/managedBeans/AdminManagedBean.java
+++ Projet_Site_Ecommerce/src/main/java/fr/adaming/managedBeans/AdminManagedBean.java
@@ -1,5 +1,26 @@
 package fr.adaming.managedBeans;
 
-public class AdminManagedBean {
+import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "aMB")
+@RequestScoped
+public class AdminManagedBean implements Serializable {
+	// Transformation de l'association UML en JAVA
+	//@ManagedProperty(value = "")
+	
+	// Setter pour l'injection de dépendance
+	
+	// Les attribut transférés à la page
+	
+	// constructeur vide
+	
+	// Getters et setters
+	
+	// les methodes metiers
+	
+	
+	
 }
